cf3e1f5aca418d204af7597dcc4f1dc2f7961fe7
WildPyromancer <lapisandsuger@gmail.com>
2020-11-19 23:22:36 +0900

    rename...shの削除。

--- autoload/resize256.sh
+++ autoload/resize256.sh
@@ -1,17 +1,19 @@
-declare -r REGEX="(\d{3,4})x(\d{3,4})"
-ls | egrep "\.jpg|\.png" | while read FILE; do
-	if [[ $(identify ${FILE}) =~ ${REGEX} ]]; then
-		echo -n $match[1] x $match[2]
-		if [ $match[1] = $match[2] ]; then
-			echo "square"
-			convert -size 256x256 ${FILE} ${FILE}
+(
+	declare -r REGEX="(\d{3,4})x(\d{3,4})"
+	find '.' -maxdepth 1 -type f | grep -e "\.jpg|\.png" | while read -r FILE; do
+		if [[ $(identify "${FILE}") =~ ${REGEX} ]]; then
+			echo -n "${match}[1] x ${match}[2]"
+			if [ "${match}[1]" = "${match}[2]" ]; then
+				echo "square"
+				convert -size 256x256 "${FILE}" "${FILE}"
+			else
+				echo "not square"
+				convert -size 256x256! "${FILE}" "${FILE}"
+			fi
+			convert -type grayscale "${FILE}" "${FILE}"
 		else
-			echo "not square"
-			convert -size 256x256! ${FILE} ${FILE}
+			echo "[${FILE}] may be not image FILE."
 		fi
-		convert -type grayscale ${FILE} ${FILE}
-	else
-		echo "[${FILE}] may be not image FILE."
-	fi
-done
+	done
+)
 return 0
