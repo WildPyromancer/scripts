1c6424de1367970429a0826b698123c382336070
WildPyromancer <lapisandsuger@gmail.com>
2020-12-15 22:49:54 +0900

    Update sort_functions.py

--- py/Sort_functions/sort_functions.py
+++ py/Sort_functions/sort_functions.py
@@ -43,37 +43,56 @@ def sort_functions(
     with file_path.open(mode='r') as f:
         lines = f.readlines()
 
-    # [
-    #   func_name:str,
-    #   start_line_index:int,
-    #   lines:int
-    # ]
+    class Function_string:
+        def __init__(
+            self,
+            func_name: str,
+            start_at: int
+        ):
+            self.func_name: str = func_name
+            self.lines: int = 1
+            self.start_at: int = start_at
 
-    REGEX_FUNC_START = re.compile(r'[ \t]*[\w_]+\(\)\s?{')
+    REGEX_FUNC_START = re.compile(r'[ \t]*[\w_0-9]+\(\) ?{')
+    REGEX_FUNC_NAME = re.compile(r'[ \t]*([\w_0-9]+)\(\) ?{')
     REGEX_FUNC_END = re.compile(r'[ \t]*}')
-    func_section_start_at: int = 0
-    func_section_finish_at: int = 0
-    flag_in_func: bool = False
+    functions: List[Function_string] = []
+    current_func: Function_string
+    function_section_start_at: int = 0
+    function_section_finish_at: int = 0
+    in_func: bool = False
+
     for line in enumerate(lines):
+        if re.match(REGEX_FUNC_START, line[1]) and not in_func:
+            if function_section_start_at == 0:
+                function_section_start_at = line[0]
 
-        if re.match(REGEX_FUNC_START, line[1]) and not flag_in_func:
-            flag_in_func = True
-            print(f'Start func at line {line[0]}')
-            print(line[1])
-            if func_section_start_at == 0:
-                func_section_start_at = line[0]
+            in_func = True
+            current_func = Function_string(
+                re.sub(REGEX_FUNC_NAME, r'\1', line[1]),
+                line[0]
+            )
+            continue
 
-        if re.match(REGEX_FUNC_END, line[1]) and flag_in_func:
-            print(f'End func at line {line[0]}')
-            print(line[1])
-            flag_in_func = False
-            func_section_finish_at = line[0]
+        if not in_func:
+            continue
+
+        current_func.lines += 1
+
+        if re.match(REGEX_FUNC_END, line[1]):
+            in_func = False
+            function_section_finish_at = line[0]
+            functions.append(current_func)
 
     print(EOF(f'''
-        {lines[0]}
-        {func_section_start_at=}
-        {func_section_finish_at=}
+        {function_section_start_at=}
+        {function_section_finish_at=}
     '''))
+
+    # def sort(func: Function_string):
+    #     pass
+
+    sorted_functions = sorted(functions, key=lambda f: f.func_name)
 
     print_center(f' {sys._getframe().f_code.co_name}() ')
     return True
